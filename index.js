d2af8c45861a33314b5aed76f1d27b53be3dc72e
unknown <you@example.com>
2023-10-18 01:54:43 +0600

    bassic sateup data server side

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middle ware
+
+app.use(cors())
+app.use(express.json())
+
+
+
+// 
+app.get('/', (req, res) => {
+    res.send('app is running')
+})
+
+app.listen(port, () => {
+    console.log(`app is running port on : ${port}`)
+})
